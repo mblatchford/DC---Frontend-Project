d97a048d0df473d9043a1e6ca38c922cdfc81b54
Michael Blatchford <mblatchford@mac.com>
2018-10-23 15:02:41 -0400

    Randomized image selection on subsequent calls of the same search query

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ function getImages(queryStr){
     // debugger;
      // fetch photos from api dynamically based on query 
     // abstracted accessKey into seperate file
-    const URI = `https://api.unsplash.com/search/photos/?query=${queryStr}&orientation=squarish&page=1&per_page=8&client_id=${accessKey}`;
+    const URI = `https://api.unsplash.com/search/photos/?query=${queryStr}&orientation=squarish&page=1&per_page=100&client_id=${accessKey}`;
     fetch(URI, {
     headers: {
         'Content-Type': 'application/json',
@@ -21,8 +21,21 @@ function getImages(queryStr){
 
 // loop json array, extract 8 small images, append attribution data
 function packageImg(jsonData){
+    console.table(jsonData.results);
+    //a given query will return the same images from the api
+    //randomizing the images taken will allow for a better user experience accross
+    //games with the same img query 
+    console.log(jsonData.results.length);
+    function getRandomIntInclusive(min=0, max=jsonData.results.length-1) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+      }
+
     let imgArray = [];
     for(let i = 0; i < 8; i++ ){
+        // new randomized index with each loop
+        let randomized = getRandomIntInclusive();
         // keep reintializing a new imgObj in the loop
         // else if outside, just accesing memory pointers to one object
         let imgObj = {
@@ -30,11 +43,11 @@ function packageImg(jsonData){
             attr_name: "",
             attr_url: ""
         };
-       imgObj.src = `${jsonData.results[i].urls.small}`;
-       imgObj.attr_name = `${jsonData.results[i].user.name}`;
-       imgObj.attr_url = `${jsonData.results[i].links.html}`;
+       imgObj.src = `${jsonData.results[randomized].urls.small}`;
+       imgObj.attr_name = `${jsonData.results[randomized].user.name}`;
+       imgObj.attr_url = `${jsonData.results[randomized].links.html}`;
        imgArray.push(imgObj);
     }
-    // console.table(imgArray);
+    console.table(imgArray);
     return imgArray;
 }
