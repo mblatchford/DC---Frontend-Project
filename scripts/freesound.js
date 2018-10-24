378db48e5daa6019109a2dda59f90fda88090316
Michael Blatchford <mblatchford@mac.com>
2018-10-24 10:17:07 -0400

    Fetch sound files based on parameter input, returns soundObj

---
+++ scripts/freesound.js
@@ -0,0 +1,59 @@
+// query is checked ahead of time and coded to button init
+let queryStr = "cartoon";
+// durationEnd allows for searching longer or shorter effects
+// may not be necessary in final build, but can be dynamically created
+let durationEnd = 3;
+// pageSize should increase as level size increases so not to pull
+// down unnecessary amounts of data. Should leave enough overhead
+// for some randomization though
+let pageSize = 10;
+
+let numSounds = 8;
+
+function getSounds(queryStr,durationEnd,pageSize){
+    // fetch sounds from api dynamically based on query 
+    // include parameter for duration match in seconds
+    // include parameter of pageSize for num results per fetch
+    // abstracted accessKey into seperate file
+     const URI = `https://freesound.org/apiv2/search/text/?query=${queryStr}&page_size=${pageSize}&filter=duration:[1 TO ${durationEnd}]&fields=name,url,id,username,previews&token=${accessKeyFreesound}`;
+   
+    fetch(URI)
+    //stringified data needs to be converted to json for use
+    .then(str => str.json())
+    //pass json to sound extractor
+    .then(packageSoundID)
+    // .then(pass to next function)
+}
+
+// loop json array, extract 8 small sounds, append attribution data
+function packageSoundID(jsonData){
+    // debugger;
+    //a given query will return the same sounds from the api
+    //randomizing the sounds taken will allow for a better user experience accross
+    //games with the same query 
+
+    function getRandomIntInclusive(min=0, max=jsonData.results.length-1) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+      }
+    let soundsArray = [];
+    for(let i = 0; i < 8; i++ ){
+        // new randomized index with each loop
+        let randomized = getRandomIntInclusive();
+        let soundObj = {
+            id: "",
+            attr_name: "",
+            attr_url: "",
+            soundFile: ""
+        }
+        soundObj.id         =`${jsonData.results[randomized].id}`;
+        soundObj.attr_name  =`${jsonData.results[randomized].username}`;
+        soundObj.attr_url   =`${jsonData.results[randomized].url}`;
+        soundObj.soundFile  =`${jsonData.results[randomized].previews["preview-hq-mp3"]}`;
+        soundsArray.push(soundObj);
+    }
+    //uncomment the below to get a handle on what is being passed back to the promise chain
+    console.table(soundsArray);
+    return soundsArray;
+}
---
+++ scripts/unsplash.js
@@ -0,0 +1,52 @@
+// let queryStr = "planets";
+
+function getImages(queryStr){
+    // debugger;
+     // fetch photos from api dynamically based on query 
+    // abstracted accessKey into seperate file
+    const URI = `https://api.unsplash.com/search/photos/?query=${queryStr}&orientation=squarish&page=1&per_page=100&client_id=${accessKeyUnsplash}`;
+    fetch(URI, {
+    headers: {
+        'Content-Type': 'application/json',
+        // api reccomended this addition
+        'Accept-Version' : 'v1'
+      }
+    })
+    //stringified data needs to be converted to json for use
+    .then(str => str.json())
+    //pass json to image extractor
+    .then(packageImg)
+    //.then(next function call -> @ian)
+}
+
+// loop json array, extract 8 small images, append attribution data
+function packageImg(jsonData){
+    //a given query will return the same images from the api
+    //randomizing the images taken will allow for a better user experience accross
+    //games with the same img query 
+    function getRandomIntInclusive(min=0, max=jsonData.results.length-1) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+      }
+
+    let imgArray = [];
+    for(let i = 0; i < 8; i++ ){
+        // new randomized index with each loop
+        let randomized = getRandomIntInclusive();
+        // keep reintializing a new imgObj in the loop
+        // else if outside, just accesing memory pointers to one object
+        let imgObj = {
+            src: "",
+            attr_name: "",
+            attr_url: ""
+        };
+       imgObj.src = `${jsonData.results[randomized].urls.small}`;
+       imgObj.attr_name = `${jsonData.results[randomized].user.name}`;
+       imgObj.attr_url = `${jsonData.results[randomized].links.html}`;
+       imgArray.push(imgObj);
+    }
+    //uncomment the below to get a handle on what is being passed back to the promise chain
+    // console.table(imgArray);
+    return imgArray;
+}
