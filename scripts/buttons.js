285bdf3c3f3da3bc4f363409ea0dca3d55682c3a
Michael Blatchford <mblatchford@mac.com>
2018-10-26 13:13:30 -0400

    Split init buttons into buttons.js

---
+++ scripts/buttons.js
@@ -0,0 +1,34 @@
+
+// ===========================================
+//  game intializing buttons
+// ===========================================
+const spaceElement = document.querySelector('[data-space]');
+const dogElement = document.querySelector('[data-dogs]');
+const catElement = document.querySelector('[data-cats]');
+
+spaceElement.addEventListener('click', () => {
+  queryStr = 'space';
+  pageSize = 100;
+  numImages = 2;
+  numSounds = 2;
+  durationEnd = 4;
+  button(queryStr, pageSize,  numImages, numSounds, durationEnd);
+});
+
+dogElement.addEventListener('click', () => {
+  queryStr = 'dog';
+  pageSize = 100;
+  numImages = 2;
+  numSounds = 2;
+  durationEnd = 4;
+  button(queryStr, pageSize,  numImages, numSounds, durationEnd);
+});
+
+catElement.addEventListener('click', () => {
+  queryStr = 'cat';
+  pageSize = 100;
+  numImages = 2;
+  numSounds = 2;
+  durationEnd = 4;
+  button(queryStr, pageSize,  numImages, numSounds, durationEnd);
+});
