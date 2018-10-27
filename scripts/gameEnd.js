5de36b517d9911d52586384de2d6449322f76a44
Michael Blatchford <mblatchford@mac.com>
2018-10-27 11:30:09 -0400

    Moved timer from H1 to standalone div, zero out timer display on a win

--- scripts/gameEnd.js
+++ scripts/gameEnd.js
@@ -1,9 +1,12 @@
 function gameEnd(end){
     if(end === "win"){
         //write new message, call modal, delete game
+        let timer = document.querySelector('.timer');
+        timer.textContent = "Time Remaining: " + "00:00";
         modalTextElement.textContent = "You're a winner!"; 
         modalElement.classList.toggle('modal-hidden');
         document.querySelector('[data-board]').innerHTML = "";
+
 
     }else if(end === "loss"){
         modalTextElement.textContent = "You're a loser!"; 
