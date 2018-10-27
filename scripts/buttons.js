3ff2d307183e4039dcba3b9d54269efee636cee5
Ian Hundere <ianfundere@gmail.com>
2018-10-27 13:10:35 -0400

    adds howto instructions

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,37 +1,36 @@
-
 // ===========================================
 //  game intializing buttons
 // ===========================================
-const spaceElement = document.querySelector('[data-space]');
-const dogElement = document.querySelector('[data-dogs]');
-const catElement = document.querySelector('[data-cats]');
+const spaceElement = document.querySelector("[data-space]");
+const dogElement = document.querySelector("[data-dogs]");
+const catElement = document.querySelector("[data-cats]");
 
-spaceElement.addEventListener('click', () => {
-  queryStr = 'space';
+spaceElement.addEventListener("click", () => {
+  queryStr = "space";
   pageSize = 100;
   numImages = 8;
   numSounds = 8;
   durationEnd = 4;
-  modalElement.classList.toggle('modal-hidden')
-  button(queryStr, pageSize,  numImages, numSounds, durationEnd);
+  modalElement.classList.toggle("modal-hidden");
+  button(queryStr, pageSize, numImages, numSounds, durationEnd);
 });
 
-dogElement.addEventListener('click', () => {
-  queryStr = 'dog';
+dogElement.addEventListener("click", () => {
+  queryStr = "dog";
   pageSize = 100;
   numImages = 16;
   numSounds = 16;
   durationEnd = 4;
-  modalElement.classList.toggle('modal-hidden')
-  button(queryStr, pageSize,  numImages, numSounds, durationEnd);
+  modalElement.classList.toggle("modal-hidden");
+  button(queryStr, pageSize, numImages, numSounds, durationEnd);
 });
 
-catElement.addEventListener('click', () => {
-  queryStr = 'cat';
+catElement.addEventListener("click", () => {
+  queryStr = "cat";
   pageSize = 100;
   numImages = 2;
   numSounds = 2;
   durationEnd = 4;
-  modalElement.classList.toggle('modal-hidden')
-  button(queryStr, pageSize,  numImages, numSounds, durationEnd);
+  modalElement.classList.toggle("modal-hidden");
+  button(queryStr, pageSize, numImages, numSounds, durationEnd);
 });
--- scripts/gameEnd.js
+++ scripts/gameEnd.js
@@ -1,18 +1,14 @@
-function gameEnd(end){
-    if(end === "win"){
-        //write new message, call modal, delete game
-        let timer = document.querySelector('.timer');
-        timer.textContent = "Time Remaining: " + "00:00";
-        modalTextElement.textContent = "You're a winner!"; 
-        modalElement.classList.toggle('modal-hidden');
-        document.querySelector('[data-board]').innerHTML = "";
-
-
-    }else if(end === "loss"){
-        modalTextElement.textContent = "You're a loser!"; 
-        modalElement.classList.toggle('modal-hidden');
-        document.querySelector('[data-board]').innerHTML = "";
-
-    }
-
+function gameEnd(end) {
+  if (end === "win") {
+    //write new message, call modal, delete game
+    let timer = document.querySelector(".timer");
+    timer.textContent = "Time Remaining: " + "00:00";
+    modalTextElement.textContent = "Congratulations, you won!";
+    modalElement.classList.toggle("modal-hidden");
+    document.querySelector("[data-board]").innerHTML = "";
+  } else if (end === "loss") {
+    modalTextElement.textContent = "Game Over";
+    modalElement.classList.toggle("modal-hidden");
+    document.querySelector("[data-board]").innerHTML = "";
+  }
 }
