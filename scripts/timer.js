21a596a169a7c6724d3d4504d238e256b29a22c2
Ian Hundere <ianfundere@gmail.com>
2018-10-28 10:06:27 -0400

    fix merge conflicts

--- scripts/timer.js
+++ scripts/timer.js
@@ -3,74 +3,33 @@
 let interval = "";
 
 function startTimer(duration, display) {
-    let timer = duration, minutes, seconds;
-    // assigning setInterval to a variable means I can use the variable as the intervalID
-    // clearInterval cancels intervals by ID
-    interval = setInterval(function () {
-                    minutes = parseInt(timer / 60, 10)
-                    seconds = parseInt(timer % 60, 10);
+  let timer = duration,
+    minutes,
+    seconds;
+  // assigning setInterval to a variable means I can use the variable as the intervalID
+  // clearInterval cancels intervals by ID
+  interval = setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
-                    minutes = minutes < 10 ? "0" + minutes : minutes;
-                    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-                    display.textContent = " Level " + level + " Time Remaining: " + minutes + ":" + seconds;
-                    // at timer end, game ends
-                    if (--timer < 0 ){ 
-                    // stop interval from repeating
-                    clearInterval(interval);
-                    // gameEnd
-                    let loss = 'loss';
-                    gameEnd(loss);
-                    }
-
-                }, 1000);
-  }
-  
-
-function timer () {
-    // //shorter for testing only
-    // switch(level){
-    //     case 1:
-    //         time = .4;
-    //         break;
-    //     case 2:
-    //         time = .5;
-    //         break;
-    //     case 3:
-    //         time = 1;
-    //         break;
-    //     case 4:
-    //         time = 3;
-    //         break;
-    //     case 5:
-    //         time = 3;
-    //         break;
-    //     default:
-    //         console.error("level was not valid");
-    //         break;
-    // }
-    switch(level){
-        case 1:
-            time = 1;
-            break;
-        case 2:
-            time = 1.25;
-            break;
-        case 3:
-            time = 1.5;
-            break;
-        case 4:
-            time = 1.75;
-            break;
-        case 5:
-            time = 2;
-            break;
-        default:
-            console.error("level was not valid");
-            break;
+    display.textContent = "Time Remaining: " + minutes + ":" + seconds;
+    // at timer end, game ends
+    if (--timer < 0) {
+      // stop interval from repeating
+      clearInterval(interval);
+      // gameEnd
+      let loss = "loss";
+      gameEnd(loss);
     }
+  }, 1000);
+}
 
-    let minutes = 60 * time,
-    display = document.querySelector('.timer');
-    startTimer(minutes, display);
-};
+//  timer includes default if no parameter is passed
+function timer(time = 0.2) {
+  let minutes = 60 * time,
+    display = document.querySelector(".timer");
+  startTimer(minutes, display);
+}
