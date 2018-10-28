6e6f6b978faa4c82f2ef248c54498841d4058a1c
Edward Whelan <vroxing@gmail.com>
2018-10-28 11:42:33 -0400

    Revert "fix merge conflicts"
    This reverts commit 21a596a169a7c6724d3d4504d238e256b29a22c2.

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,36 +1,39 @@
+
 // ===========================================
 //  game intializing buttons
 // ===========================================
-const spaceElement = document.querySelector("[data-space]");
-const dogElement = document.querySelector("[data-dogs]");
-const catElement = document.querySelector("[data-cats]");
+const spaceElement = document.querySelector('[data-space]');
+const dogElement = document.querySelector('[data-dogs]');
+const catElement = document.querySelector('[data-cats]');
+let level = 1;
 
-spaceElement.addEventListener("click", () => {
-  queryStr = "space";
+// why don't these parameters need to be defined with a const or let? They aren't globally scoped right?
+spaceElement.addEventListener('click', () => {
+  queryStr = 'space';
   pageSize = 100;
-  numImages = 8;
-  numSounds = 8;
-  durationEnd = 4;
-  modalElement.classList.toggle("modal-hidden");
-  button(queryStr, pageSize, numImages, numSounds, durationEnd);
+  numImages = 6 + (level*2);
+  numSounds = 6 + (level*2);
+  durationEnd = 3;
+  modalElement.classList.toggle('modal-hidden')
+  button(queryStr, pageSize,  numImages, numSounds, durationEnd);
 });
 
-dogElement.addEventListener("click", () => {
-  queryStr = "dog";
+dogElement.addEventListener('click', () => {
+  queryStr = 'dog';
   pageSize = 100;
-  numImages = 16;
-  numSounds = 16;
-  durationEnd = 4;
-  modalElement.classList.toggle("modal-hidden");
-  button(queryStr, pageSize, numImages, numSounds, durationEnd);
+  numImages = 6 + (level*2);
+  numSounds = 6 + (level*2);
+  durationEnd = 3;
+  modalElement.classList.toggle('modal-hidden')
+  button(queryStr, pageSize,  numImages, numSounds, durationEnd);
 });
 
-catElement.addEventListener("click", () => {
-  queryStr = "cat";
+catElement.addEventListener('click', () => {
+  queryStr = 'cat';
   pageSize = 100;
-  numImages = 2;
-  numSounds = 2;
-  durationEnd = 4;
-  modalElement.classList.toggle("modal-hidden");
-  button(queryStr, pageSize, numImages, numSounds, durationEnd);
+  numImages = 6 + (level*2);
+  numSounds = 6 + (level*2);
+  durationEnd = 3;
+  modalElement.classList.toggle('modal-hidden')
+  button(queryStr, pageSize,  numImages, numSounds, durationEnd);
 });
--- scripts/unsplash.js
+++ scripts/unsplash.js
@@ -8,65 +8,63 @@
 // let numImages = 8;
 
 function getImages(queryStr, pageSize, numImages) {
-  // fetch photos from api dynamically based on query
-  // abstracted accessKey into seperate file
-  const URI = `https://api.unsplash.com/search/photos/?query=${queryStr}&orientation=squarish&page=1&per_page=${pageSize}&client_id=${accessKeyUnsplash}`;
-  return (
-    fetch(URI, {
-      headers: {
-        "Content-Type": "application/json",
-        // api reccomended this addition
-        "Accept-Version": "v1"
-      }
+    // fetch photos from api dynamically based on query 
+    // abstracted accessKey into seperate file
+    const URI = `https://api.unsplash.com/search/photos/?query=${queryStr}&orientation=squarish&page=1&per_page=${pageSize}&client_id=${accessKeyUnsplash}`;
+    return fetch(URI, {
+        headers: {
+            'Content-Type': 'application/json',
+            // api reccomended this addition
+            'Accept-Version': 'v1'
+        }
     })
-      //stringified data needs to be converted to json for use
-      .then(str => {
-        return Promise.all([str.json(), numImages]);
-      })
-      //pass json to image extractor
-      .then(packageImg)
-  );
+        //stringified data needs to be converted to json for use
+        .then(str => {
+            return Promise.all([str.json(), numImages]);
+        })
+        //pass json to image extractor
+        .then(packageImg)
 }
 
 // loop json array, extract small images, append attribution data
 function packageImg([jsonData, numImages]) {
-  //a given query will return the same images from the api
-  //randomizing the images taken will allow for a better user experience accross
-  //games with the same query
-  function getRandomIntInclusive(min = 0, max = jsonData.results.length - 1) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-  }
-
-  //non-repeating number logic
-  let rand = [];
-  while (rand.length < numImages) {
-    let num = getRandomIntInclusive();
-    // ! reverses logic
-    // .includes would return true if found
-    // so if num not found add it else try again
-    if (!rand.includes(num)) {
-      rand.push(num);
+    //a given query will return the same images from the api
+    //randomizing the images taken will allow for a better user experience accross
+    //games with the same query 
+    function getRandomIntInclusive(min = 0, max = jsonData.results.length - 1) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
     }
-  }
-  // console.log(`rand[] ${rand}`);
-  let imgArray = [];
-  rand.forEach(index => {
-    // keep reintializing a new imgObj in the loop
-    // else if outside, just accesing memory pointers to one object
-    let imgObj = {
-      imgSrc: "",
-      attrImg_name: "",
-      attrImg_url: ""
-    };
-    imgObj.imgSrc = `${jsonData.results[index].urls.small}`;
-    imgObj.attrImg_name = `${jsonData.results[index].user.name}`;
-    imgObj.attrImg_url = `${jsonData.results[index].links.html}`;
-    imgArray.push(imgObj);
-  });
 
-  //uncomment the below to get a handle on what is being passed back to the promise chain
-  //console.table(imgArray);
-  return imgArray;
+    //non-repeating number logic
+    let rand = [];
+    while (rand.length < numImages) {
+        let num = getRandomIntInclusive();
+        // ! reverses logic
+        // .includes would return true if found
+        // so if num not found add it else try again
+        if (!rand.includes(num)) {
+            rand.push(num);
+        }
+    }
+    // console.log(`rand[] ${rand}`);
+    let imgArray = [];
+    rand.forEach(index => {
+        // keep reintializing a new imgObj in the loop
+        // else if outside, just accesing memory pointers to one object
+        let imgObj = {
+            imgSrc: "",
+            attrImg_name: "",
+            attrImg_url: ""
+        };
+        imgObj.imgSrc = `${jsonData.results[index].urls.small}`;
+        imgObj.attrImg_name = `${jsonData.results[index].user.name}`;
+        imgObj.attrImg_url = `${jsonData.results[index].links.html}`;
+        imgArray.push(imgObj);
+    });
+
+    //uncomment the below to get a handle on what is being passed back to the promise chain
+    //console.table(imgArray);
+    return imgArray;
 }
