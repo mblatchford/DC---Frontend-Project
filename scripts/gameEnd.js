3a0b80783ea8916998e1addba27fe6ac987c5112
Michael Blatchford <mblatchford@mac.com>
2018-10-29 17:58:14 -0400

    Fixed game ending bug

--- scripts/gameEnd.js
+++ scripts/gameEnd.js
@@ -2,9 +2,10 @@ function gameEnd(end){
     if(end === "win" && level != 5){
         //write new message, call modal, delete game
         let timer = document.querySelector('.timer');
+        let levelStatus = document.querySelector('.level');
         // timer.textContent = "Level " + level + " Time Remaining: " + "00:00";
-        level.textContent = " Level " + level;
-        timer.textContent = "Time Remaining: " + minutes + ":" + seconds;
+        level.textContent = "Level " + levelStatus;
+        timer.textContent = "Time Remaining: " + "00:00";
         modalTextElement.textContent = "You're a winner!"; 
         modalElement.classList.toggle('modal-hidden');
         document.querySelector('[data-board]').innerHTML = "";
@@ -12,6 +13,7 @@ function gameEnd(end){
     }else if(end === "win" && level === 5){
         level = 1;
         let timer = document.querySelector('.timer');
+        let level = document.querySelector('.level');
         timer.textContent = "";
         level.textContent = "";
         modalTextElement.textContent = "You've beaten the game!!!"; 
