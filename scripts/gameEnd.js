57c39f1bb5b986772d59840c7a85c4f03e873163
Michael Blatchford <mblatchford@mac.com>
2018-10-30 08:39:26 -0400

    Removal of some uncessesary code

--- scripts/gameEnd.js
+++ scripts/gameEnd.js
@@ -3,7 +3,6 @@ function gameEnd(end) {
     //write new message, call modal, delete game
     let timer = document.querySelector(".timer");
     let levelStatus = document.querySelector(".level");
-    // timer.textContent = "Level " + level + " Time Remaining: " + "00:00";
     levelStatus.textContent = "Level " + level;
     timer.textContent = "Time Remaining: " + "00:00";
     modalH2Element.textContent = "You're a winner!";
@@ -18,15 +17,12 @@ function gameEnd(end) {
     timer.textContent = "";
     levelStatus.textContent = "";
     modalH2Element.textContent = "You've beaten the game!!!";
-    modalPElement.textContent = "(but you can try again)";
+    modalPElement.textContent = "";
     modalElement.classList.toggle("modal-hidden");
-    let celebrate = document.querySelector("#particles-js");
-    celebrate.removeAttribute;
     particlesJS("particles-js", "app.js", function() {
       console.log("particles.js loaded - callback");
     });
     document.querySelector("[data-board]").innerHTML = "";
-    // game ending modal splash or transition different from the others?
   } else if (end === "loss") {
     modalH2Element.textContent = "You're a loser!";
     modalPElement.textContent = "(but you can try again)";
